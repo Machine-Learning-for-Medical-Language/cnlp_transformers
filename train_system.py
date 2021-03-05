482c3519ee2ad026cfcbe165efc3520d466ae2d1
Tim Miller <tim.miller@gmail.com>
2021-03-05 06:39:07 -0500

    Updated for new trainer parameter names

--- train_system.py
+++ train_system.py
@@ -307,7 +307,7 @@ def main():
     # Training
     if training_args.do_train:
         trainer.train(
-            model_path=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
+            resume_from_checkpoint=model_args.model_name_or_path if os.path.isdir(model_args.model_name_or_path) else None
         )
 
         if not hasattr(model, 'best_score'):
