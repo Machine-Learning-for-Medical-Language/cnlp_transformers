df7d347ccf1584082b8855d74b5bbe8b12806685
Angus L'Herrou <piraka@brandeis.edu>
2022-09-16 15:09:44 -0500

    added submodule presence tests

--- test/test_init.py
+++ test/test_init.py
@@ -9,3 +9,35 @@ def test_init():
     """
     import cnlpt
     assert cnlpt.__package__ == 'cnlpt'
+
+
+class TestSubmodulesPresent:
+    """
+    Test that all submodules are defined and can be imported
+    """
+    def test_BaselineModels_present(self):
+        import cnlpt.BaselineModels
+        assert cnlpt.BaselineModels.__package__ == 'cnlpt'
+
+    def test_cnlp_data_present(self):
+        import cnlpt.cnlp_data
+        assert cnlpt.cnlp_data.__package__ == 'cnlpt'
+
+    def test_cnlp_processors_present(self):
+        import cnlpt.cnlp_processors
+        assert cnlpt.cnlp_processors.__package__ == 'cnlpt'
+
+    def test_CnlpModelForClassification_present(self):
+        import cnlpt.CnlpModelForClassification
+        assert cnlpt.CnlpModelForClassification.__package__ == 'cnlpt'
+
+    def test_HierarchicalTransformer_present(self):
+        import cnlpt.HierarchicalTransformer
+        assert cnlpt.HierarchicalTransformer.__package__ == 'cnlpt'
+    def test_thyme_eval_present(self):
+        import cnlpt.thyme_eval
+        assert cnlpt.thyme_eval.__package__ == 'cnlpt'
+
+    def test_train_system_present(self):
+        import cnlpt.train_system
+        assert cnlpt.train_system.__package__ == 'cnlpt'
