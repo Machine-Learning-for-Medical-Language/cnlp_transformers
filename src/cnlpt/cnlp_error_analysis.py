fa6edaa9486aa81666e16228f3064362d2ea7613
Eli Goldner <egoldner@brandeis.edu>
2023-06-22 14:58:57 -0400

    More scaffolding

--- src/cnlpt/cnlp_error_analysis.py
+++ src/cnlpt/cnlp_error_analysis.py
@@ -2,33 +2,40 @@ import numpy as np
 from transformers.trainer_utils import EvalPrediction
 
 from datasets import Dataset
-from transformers import Trainer, PreTrainedTokenizer
+from transformers import Trainer, PreTrainedTokenizer, EvalPrediction
 from .cnlp_processors import tagging, relex, classification
 from .cnlp_data import ClinicalNlpDataset
-from typing import Dict
+from typing import Dict, List
 
 
-def collect_disagreements(p: EvalPrediction):
+def collect_disagreements(
+    task_names: List[str],
+    p: EvalPrediction,
+    max_seq_length: int,
+    dataset: ClinicalNlpDataset,
+):
     disagree_inds = {}
     task_label_ind = 0
 
     for task_ind, task_name in enumerate(task_names):
-        if tagger[task_name]:
+        tagger = dataset.output_modes[task_name] == tagging
+        relations = dataset.output_modes[task_name] == relex
+        if tagger:
             preds = np.argmax(p.predictions[task_ind], axis=2)
             # labels will be -100 where we don't need to tag
-        elif relations[task_name]:
+        elif relations:
             preds = np.argmax(p.predictions[task_ind], axis=3)
         else:
             preds = np.argmax(p.predictions[task_ind], axis=1)
 
-        if relations[task_name]:
+        if relations:
             # relation labels
             labels = p.label_ids[
                 :, :, task_label_ind : task_label_ind + data_args.max_seq_length
             ].squeeze()
             task_label_ind += data_args.max_seq_length
         elif p.label_ids.ndim == 3:
-            if tagger[task_name]:
+            if tagger:
                 labels = p.label_ids[
                     :, :, task_label_ind : task_label_ind + 1
                 ].squeeze()
@@ -37,7 +44,66 @@ def collect_disagreements(p: EvalPrediction):
             task_label_ind += 1
         elif p.label_ids.ndim == 2:
             labels = p.label_ids[:, task_ind].squeeze()
-        disagree_inds[task_name] = compute_disagreements()
+        disagree_inds[task_name] = compute_disagreements(
+            preds,
+            labels,
+            dataset.output_modes[task_name],
+        )
+
+
+def compute_disagreements(
+    preds: np.ndarray,
+    labels: np.ndarray,
+    output_mode: str,
+):
+    """
+    Function that defines and computes the metrics used for each task.
+
+    When adding a task definition to this file, add a branch to this
+    function defining what its evaluation metric invocation should be.
+    If the new task is a simple classification task, a sensible default
+    is defined; falling back on this will trigger a warning.
+
+    :param str task_name: the task name used to index into cnlp_processors
+    :param numpy.ndarray preds: the predicted labels from the model
+    :param numpy.ndarray labels: the true labels
+    :rtype: typing.Dict[str, typing.Any]
+    :return: a dictionary containing evaluation metrics
+    """
+
+    assert len(preds) == len(
+        labels
+    ), f"Predictions and labels have mismatched lengths {len(preds)} and {len(labels)}"
+    if output_mode == classification:
+        return classification_disagreements(preds=preds, labels=labels)
+    elif output_mode == tagging:
+        return tagging_disagreements(
+            preds=preds, labels=labels
+        )
+    elif output_mode == relex:
+        return relation_disagreements(
+            preds=preds, labels=labels,
+        )
+    else:
+        raise Exception(
+            "As yet unsupported task in cnlpt"
+        )
+
+
+def classification_disagreements(preds: np.ndarray, labels: np.ndarray):
+    (indices,) = np.where(np.not_equal(preds, labels))
+    return indices
+
+
+def tagging_disagreements(preds: np.ndarray, labels: np.ndarray):
+    (indices,) = np.where([*map(any, np.not_equal(preds, labels))])
+    return indices
+
+
+def relation_disagreements(preds: np.ndarray, labels: np.ndarray):
+    (indices,) = np.where([*map(lambda s: s.any(), np.not_equal(preds, labels))])
+    return indices
+
 
 def write_errors_for_dataset(
     output_fn: str,
