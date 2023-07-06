c45a32e97f86aaba995a8ccc025cec7fcea024ff
Angus L'Herrou <piraka@brandeis.edu>
2023-07-06 14:08:01 -0500

    add new functions is_cnlpt_model and encoder_inferred

--- src/cnlpt/train_system.py
+++ src/cnlpt/train_system.py
@@ -68,6 +68,23 @@ def is_hub_model(model_name):
 
     return False
 
+
+def is_cnlpt_model(model_path: str) -> bool:
+    """
+    Infer whether a model path refers to a cnlpt
+    model checkpoint (if not, we assume it is an
+    encoder)
+    :param model_path: the path to the model
+    :return: whether the model is a cnlpt classifier model
+    """
+    encoder_config = AutoConfig.from_pretrained(model_path)
+    return encoder_config.model_type == "cnlpt"
+
+
+def encoder_inferred(model_name_or_path: str) -> bool:
+    return is_hub_model(model_name_or_path) or not is_cnlpt_model(model_name_or_path)
+
+
 def main(json_file: Optional[str] = None, json_obj: Optional[Dict[str, Any]] = None):
     """
     See all possible arguments in :class:`transformers.TrainingArguments`
