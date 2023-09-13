18af95834daaa1dfe24816a97279da46f8a8cc41
spencerthomas1722 <spencer.thomas@childrens.harvard.edu>
2023-09-13 15:44:12 -0400

    reformat select_hyperparameters.py

--- src/cnlpt/select_hyperparameters.py
+++ src/cnlpt/select_hyperparameters.py
@@ -10,10 +10,7 @@ import itertools
 def get_job_from_hyperparameters(hyperparameters, indices):
     sorted_pairs = sorted(hyperparameters.items())
 
-    paired_lists = [
-        [(key, value) for value in values]
-        for key, values in sorted_pairs
-    ]
+    paired_lists = [[(key, value) for value in values] for key, values in sorted_pairs]
 
     if len(indices) == 1:
         index = indices[0]
@@ -22,19 +19,24 @@ def get_job_from_hyperparameters(hyperparameters, indices):
     elif len(indices) == len(paired_lists):
         return dict(pair[index] for pair, index in zip(paired_lists, indices))
     else:
-        raise ValueError(f'Expected {len(paired_lists)} indices but received {len(indices)}')
+        raise ValueError(
+            f"Expected {len(paired_lists)} indices but received {len(indices)}"
+        )
 
 
 @click.command()
-@click.argument('hyperparameters', type=click.Path(path_type=Path, exists=True, dir_okay=False))
-@click.argument('indices', type=int, nargs=-1, required=True)
-@click.option('--default-args',
-              type=click.Path(path_type=Path, exists=True, dir_okay=False),
-              default=None,
-              help="If supplied, merges the default arguments found in the provided "
-                   "file with the indexed experiment arguments. Only necessary if "
-                   "using different default arguments from what is in cnn/train.py."
-              )
+@click.argument(
+    "hyperparameters", type=click.Path(path_type=Path, exists=True, dir_okay=False)
+)
+@click.argument("indices", type=int, nargs=-1, required=True)
+@click.option(
+    "--default-args",
+    type=click.Path(path_type=Path, exists=True, dir_okay=False),
+    default=None,
+    help="If supplied, merges the default arguments found in the provided "
+    "file with the indexed experiment arguments. Only necessary if "
+    "using different default arguments from what is in cnn/train.py.",
+)
 def cli(hyperparameters: Path, indices: Tuple[int], default_args: Optional[Path]):
     """
     Produces the hyperparameter dictionary for a job index from a
@@ -44,15 +46,15 @@ def cli(hyperparameters: Path, indices: Tuple[int], default_args: Optional[Path]
     a space-separated list of numbers indexing into the list of options
     for each hyperparameter, in alphabetical order.
     """
-    with hyperparameters.open('r', encoding='utf8') as hp_file:
+    with hyperparameters.open("r", encoding="utf8") as hp_file:
         hp_dict = json.load(hp_file)
 
     if default_args is not None:
-        with open('default_args.json', 'r', encoding='utf8') as def_args_file:
+        with open("default_args.json", "r", encoding="utf8") as def_args_file:
             default_args_dict = json.load(def_args_file)
         hyperparameters = {
             **default_args_dict,
-            **get_job_from_hyperparameters(hp_dict, indices)
+            **get_job_from_hyperparameters(hp_dict, indices),
         }
     else:
         hyperparameters = get_job_from_hyperparameters(hp_dict, indices)
@@ -60,5 +62,5 @@ def cli(hyperparameters: Path, indices: Tuple[int], default_args: Optional[Path]
     click.echo(json.dumps(hyperparameters, indent=2))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
