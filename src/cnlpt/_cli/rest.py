b749b8c3c35d64ed17708f2f4d7ab6e0a9be243a
ianbulovic <ian.bulovic@gmail.com>
2024-11-20 09:31:03 -0500

    change --model to --model_type, lazy load fastapi apps

--- src/cnlpt/_cli/rest.py
+++ src/cnlpt/_cli/rest.py
@@ -1,32 +1,12 @@
 import click
 
-from ..api.cnn_rest import app as cnn_app
-from ..api.current_rest import app as current_app
-from ..api.dtr_rest import app as dtr_app
-from ..api.event_rest import app as event_app
-from ..api.hier_rest import app as hier_app
-from ..api.negation_rest import app as negation_app
-from ..api.temporal_rest import app as temporal_app
-from ..api.termexists_rest import app as termexists_app
-from ..api.timex_rest import app as timex_app
-
-APPS = {
-    "cnn": cnn_app,
-    "current": current_app,
-    "dtr": dtr_app,
-    "event": event_app,
-    "hier": hier_app,
-    "negation": negation_app,
-    "temporal": temporal_app,
-    "termexists": termexists_app,
-    "timex": timex_app,
-}
+from ..api import MODEL_TYPES, get_rest_app
 
 
 @click.command("rest", context_settings={"show_default": True})
 @click.option(
-    "--model",
-    type=click.Choice(APPS.keys()),
+    "--model-type",
+    type=click.Choice(MODEL_TYPES),
     required=True,
 )
 @click.option(
@@ -46,8 +26,8 @@ APPS = {
     default=False,
     help="Auto-reload the REST app.",
 )
-def rest_command(model: str, host: str, port: int, reload: bool):
+def rest_command(model_type: str, host: str, port: int, reload: bool):
     """Start a REST application from a model."""
     import uvicorn
 
-    uvicorn.run(APPS[model], host=host, port=port, reload=reload)
+    uvicorn.run(get_rest_app(model_type), host=host, port=port, reload=reload)
---
+++ src/cnlpt/api/__init__.py
@@ -0,0 +1,52 @@
+MODEL_TYPES = (
+    "cnn",
+    "current",
+    "dtr",
+    "event",
+    "hier",
+    "negation",
+    "temporal",
+    "termexists",
+    "timex",
+)
+
+
+def get_rest_app(model_type: str):
+    if model_type == "cnn":
+        from .cnn_rest import app
+
+        return app
+    elif model_type == "current":
+        from .current_rest import app
+
+        return app
+    elif model_type == "dtr":
+        from .dtr_rest import app
+
+        return app
+    elif model_type == "event":
+        from .event_rest import app
+
+        return app
+    elif model_type == "hier":
+        from .hier_rest import app
+
+        return app
+    elif model_type == "negation":
+        from .negation_rest import app
+
+        return app
+    elif model_type == "temporal":
+        from .temporal_rest import app
+
+        return app
+    elif model_type == "termexists":
+        from .termexists_rest import app
+
+        return app
+    elif model_type == "timex":
+        from .timex_rest import app
+
+        return app
+    else:
+        raise ValueError(f"unknown model type: {model_type}")
